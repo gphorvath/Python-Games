3549f257bff4702ee4602730bc10919f55740250
Gregory Horvath <gphorvath@gameshenanigans.com>
2024-03-24 19:00:49 -0400

    working on blackjack game

---
+++ Blackjack/art.py
@@ -0,0 +1,180 @@
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
+
+def concatenate_cards(*cards):
+    concatenated_cards = ""
+    for lines in zip(*[card.split("\n") for card in cards]):
+        concatenated_cards += "  ".join(lines) + "\n"
+    return concatenated_cards
+
+def get_card(digit_and_suit):
+    if len(digit_and_suit) == 3:
+        digit = digit_and_suit[0] + digit_and_suit[1]
+        suit = digit_and_suit[2]
+    elif len(digit_and_suit) == 2:
+        digit, suit = digit_and_suit
+    else:
+        return f"Invalid card: {digit_and_suit}"
+    
+    if suit == "♠":
+        return spades(digit)
+    elif suit == "♦":
+        return diamonds(digit)
+    elif suit == "♥":
+        return hearts(digit)
+    elif suit == "♣":
+        return clubs(digit)
+    else:
+        return f"Invalid suit: {suit}"
+    
+def card_back_base():
+    return """
+.------.
+|░░░░░░|
+|░░░░░░|
+|░░░░░░|
+|░░░░░░|
+'------'
+"""
+
+def card_back():
+    return """
+.------.
+|░GAME░|
+|░SHE░░|
+|░░NAN░|
+|░IGANS|
+'------'
+"""
+
+def spades(digit):
+    digit = str(digit)
+    if(len(digit) == 1):
+        return f""" 
+.------.
+|{digit}     |
+|  /\  |
+| (__) |
+|  /\ {digit}|
+'------'
+"""
+    elif(len(digit) == 2):
+        return f""" 
+.------.
+|{digit}    |
+|  /\  |
+| (__) |
+|  /\{digit}|
+'------'
+"""
+    else:
+        return f"Invalid digit: {digit}"
+
+def diamonds(digit):
+    digit = str(digit)
+    if(len(digit) == 1):
+        return f""" 
+.------.
+|{digit} /\  |
+| /  \ |
+| \  / |
+|  \/ {digit}|
+'------'
+"""
+    elif(len(digit) == 2):
+        return f""" 
+.------.
+|{digit}/\  |
+| /  \ |
+| \  / |
+|  \/{digit}|
+'------'
+"""
+    else:
+        return f"Invalid digit: {digit}"
+    
+def hearts(digit):
+    digit = str(digit)
+    if(len(digit) == 1):
+        return f""" 
+.------.
+|{digit}_  _ |
+|( \/ )|
+| \  / |
+|  \/ {digit}|
+'------'
+"""
+    elif(len(digit) == 2):
+        return f""" 
+.------.
+|{digit}  _ |
+|( \/ )|
+| \  / |
+|  \/{digit}|
+'------'
+"""
+    else:
+        return f"Invalid digit: {digit}"
+
+def clubs(digit):
+    digit = str(digit)
+    if(len(digit) == 1):
+        return f""" 
+.------.
+|{digit} _   |
+| (_)  |
+|(_x_) |
+|  Y  {digit}|
+'------'
+"""
+    elif(len(digit) == 2):
+        return f""" 
+.------.
+|{digit}_   |
+| (_)  |
+|(_x_) |
+|  Y {digit}|
+'------'
+"""
+    else:
+        return f"Invalid digit: {digit}"
+    
+bust = """
+ _               _   
+| |             | |  
+| |__  _   _ ___| |_ 
+| '_ \| | | / __| __|
+| |_) | |_| \__ \ |_ 
+|_.__/ \__,_|___/\__|
+"""
+
+win = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠚⠙⠲⢤⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠴⢊⣥⣤⣤⣤⣶⣦⣍⠓⠦⣄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣦⣈⠙⠻⢿⣿⡛⣻⣿⣿⣿⠂⠈⠙⢦⡄⠀
+⠀⠀⠀⠀⠀⠀⢀⡀⠀⠠⣶⣿⣿⣿⣿⣿⣶⣤⣈⠙⠻⢿⡿⠟⣀⠴⢚⣻⠆⠀
+⠀⠀⠀⠀⡠⠔⣩⣴⣿⣦⣄⠙⠻⢿⣿⣿⣿⣿⣿⣿⡶⠀⠀⢋⡡⠖⢉⡽⠂⠀
+⠀⢠⣔⠋⠀⠀⣿⣿⣿⡟⠛⣿⣶⣤⡈⠛⠿⡿⠟⢁⣤⣾⠀⢉⡤⠚⣡⠝⠃⠀
+⠀⠤⣎⡙⠲⣌⡛⠿⣿⣿⣿⣿⣿⡿⠟⣃⠤⠀⣾⣿⣿⣿⠀⣁⠔⠊⠀⠀⠀⠀
+⠀⠐⢿⣉⠓⠦⣍⡒⠬⣁⠀⢀⣠⠔⣋⠵⠂⠀⣿⣿⣿⠟⠀⠁⠀⠀⠀⠀⠀⠀
+⠀⠘⠳⣌⡙⠲⣄⡙⠲⢬⣉⣉⠴⠋⣡⠔⢋⠀⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠙⠲⢤⣉⠓⢦⣈⣡⠖⢋⡠⠖⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠓⠦⣌⡤⠖⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+
+lose = """
+_____.___.              .____                        
+\__  |   | ____  __ __  |    |    ____  ______ ____  
+ /   |   |/  _ \|  |  \ |    |   /  _ \/  ___// __ \ 
+ \____   (  <_> )  |  / |    |__(  <_> )___ \\  ___/ 
+ / ______|\____/|____/  |_______ \____/____  >\___  >
+ \/                             \/         \/     \/ 
+"""
---
+++ Blackjack/game.py
@@ -0,0 +1,113 @@
+import random
+import os
+from art import (
+    logo,
+    get_card,
+    concatenate_cards,
+    card_back,
+    bust,
+    win,
+    lose,
+)
+
+def new_deck():
+    return [
+        "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♠",
+        "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♦",
+        "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♥",
+        "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣", "A♣"
+    ]
+
+def shuffle_deck(deck):
+    random.shuffle(deck)
+
+def deal_card(deck):
+    return deck.pop()
+
+def hand_value(cards):
+    total = 0
+    ace = False
+    for card in cards:
+        if card[0] in "JQK":
+            total += 10
+        elif card[0] == "A":
+            ace = True
+        else:
+            total += int(card[:-1])
+    if ace:
+        if total + 11 <= 21:
+            total += 11
+        else:
+            total += 1
+    return total
+
+
+def print_game_state(player_hand, dealer_hand):
+    os.system("clear")
+    print(logo)
+    
+    if(len(dealer_hand) == 1):
+        print(f"Dealer Showing: {hand_value(dealer_hand)}")
+        print(concatenate_cards(get_card(dealer_hand[0]), card_back()))
+    else:
+        print(f"Dealer: {hand_value(dealer_hand)}")
+        print(concatenate_cards(*[get_card(card) for card in dealer_hand]))
+    print(f"Player: {hand_value(player_hand)}")
+    print(concatenate_cards(*[get_card(card) for card in player_hand]))
+    
+
+
+def game_loop():
+    deck = new_deck()
+    shuffle_deck(deck)
+    dealer_hand1 = []
+    player_hand1 = []
+    
+    # Starting hands
+    dealer_hand1.append(deal_card(deck))
+    player_hand1.append(deal_card(deck))
+    player_hand1.append(deal_card(deck))
+    print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)
+    
+    while hand_value(player_hand1) < 21:
+        action = input("Do you want to hit or stand? ").lower()
+        if action == "hit":
+            player_hand1.append(deal_card(deck))
+            print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)
+        elif action == "stand":
+            break
+        else:
+            print("Invalid action")
+            
+    if hand_value(player_hand1) > 21:
+        print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)
+        print(bust)
+    else:
+        while hand_value(dealer_hand1) < 17:
+            dealer_hand1.append(deal_card(deck))
+            print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)            
+        if hand_value(dealer_hand1) > 21:
+            print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)
+            print(win)
+            print("Dealer busts! You win!")
+        elif hand_value(dealer_hand1) > hand_value(player_hand1):
+            print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)
+            print(lose)
+            print("Dealer wins!")
+        elif hand_value(dealer_hand1) < hand_value(player_hand1):
+            print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)
+            print(win)
+            print("You win!")
+        else:
+            print_game_state(player_hand=player_hand1, dealer_hand=dealer_hand1)
+            print("It's a tie!")
+    
+    if(input("Do you want to play again? (yes or no): ").lower() == "yes"):
+        game_loop()
+    else:
+        print("Thanks for playing!")
+        return
+    
+    
+game_loop()
+
