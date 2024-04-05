1c0c04dbd2cb61458079ac59bfe104fa7ffe83f3
Gregory Horvath <gphorvath@gameshenanigans.com>
2024-04-04 23:04:31 -0400

    tightening up Hangman a bit

--- Hangman/game.py
+++ Hangman/game.py
@@ -8,41 +8,54 @@ from art_pack import (
 )
 from words import word_list
 
-lives = len(stages) - 1
-answer = random.choice(word_list)
-display = []
+def game_loop():
 
-for _ in answer:
-    display += "_"
+    lives = len(stages) - 1
+    answer = random.choice(word_list)
+    display = []
+    wrong_guesses = []
 
-game_over = False
+    for _ in answer:
+        display += "_"
 
-while not game_over:
-    os.system("clear")
-    print(logo)
-    print(stages[lives])
-    print(display)
-    print("\n")
-    letter = input("Choose a letter: ").lower()
-    
-    for position in range(len(answer)):
-        if answer[position] == letter:
-            display[position] = letter
+    game_over = False
 
-    if not letter in answer:
-        lives -= 1
+    while not game_over:
+        os.system("clear")
+        print(logo)
+        print(stages[lives])
+        print(display)
+        print("\n")
+        print(f"Wrong gueses: {wrong_guesses}\n")
+        letter = input("Choose a letter: ").lower()
         
-    if "_" not in display:
-        os.system("clear")
-        print("Thank you for playing Hangman!")
-        print("The answer was: ", answer)
-        print(win_stage)
-        print("You escape to play another day!")
-        break
-    elif lives == 0:
-        os.system("clear")
-        print("Thank you for playing Hangman!")
-        print("The answer was: ", answer)
-        print(lose_stage)
-        print("You are hanged!")
-        break
+        for position in range(len(answer)):
+            if answer[position] == letter:
+                display[position] = letter
+
+        if not letter in answer and not letter in wrong_guesses:
+            lives -= 1
+            wrong_guesses.append(letter)
+            
+        if "_" not in display:
+            os.system("clear")
+            print("Thank you for playing Hangman!")
+            print("The answer was: ", answer)
+            print(win_stage)
+            print("You escape to play another day!")
+            game_over = True
+        elif lives == 0:
+            os.system("clear")
+            print("Thank you for playing Hangman!")
+            print("The answer was: ", answer)
+            print(lose_stage)
+            print("You are hanged!")
+            game_over = True
+    
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    
+    if "y" in play_again:
+        return game_loop()
+            
+if __name__ == "__main__":
+    game_loop()
